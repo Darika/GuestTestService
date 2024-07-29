d1e4987e05b66ec0032d87c82780da2a65463681
darika <darikapiterski@gmail.com>
2024-07-29 17:01:58 +0300

    fix: (GuestTestService) добавление правила валидации на уникальность телефона и почты при редактировании гостя

--- app/Http/Requests/GuestsUpdateRequest.php
+++ app/Http/Requests/GuestsUpdateRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class GuestsUpdateRequest extends FormRequest
 {
@@ -17,9 +20,21 @@ class GuestsUpdateRequest extends FormRequest
             'id' => 'required|exists:guests,id',
             'name' => 'required|string',
             'surname' => 'required|string',
-            'phone' => 'required|string',
-            'email' => 'required|email',
+            'phone' => ['required', 'string', Rule::unique('guests', 'phone')->ignore($this->id)],
+            'email' => ['required', 'email', Rule::unique('guests', 'email')->ignore($this->id)],
             'country' => 'string|nullable'
         ];
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    public function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'error',
+            'errorMessage' => $validator->errors()->getMessages(),
+        ]));
     }
 }
